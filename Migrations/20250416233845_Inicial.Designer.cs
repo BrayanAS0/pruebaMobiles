6405d9b9d64459c3e69fc1b9fed865b32cf52d26
BrayanAS0 <el.el.brayan16@gmail.com>
2025-04-16 17:40:01 -0600

    correcto2

--- Migrations/20250416233845_Inicial.Designer.cs
+++ Migrations/20250416233845_Inicial.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using pruebaMobiles.data;
 
@@ -10,9 +11,11 @@ using pruebaMobiles.data;
 namespace pruebaMobiles.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250416233845_Inicial")]
+    partial class Inicial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
