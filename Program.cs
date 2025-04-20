57ec789c6dec575956942c3fdedc0adf80b03c8d
BrayanAS0 <el.el.brayan16@gmail.com>
2025-04-20 17:33:04 -0600

    nuevo

--- Program.cs
+++ Program.cs
@@ -3,19 +3,17 @@ using pruebaMobiles.data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 builder.Services.AddSwaggerGen();
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(8080); 
-});
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
